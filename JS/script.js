ea839bb414cd3b42e12dc78497fd85e8940b3ccc
Agitik <62553989+Agitik@users.noreply.github.com>
2020-11-09 01:25:40 +0300

    Исправлено: 1) Сохранение таблицы

--- JS/script.js
+++ JS/script.js
@@ -1,16 +1,17 @@
+const MIN_Y = -5;
+const MAX_Y = 3;
+var rParametr = 0;
+
 function addString(result) {
   if(result != null){
     table = $('#ResultTable')
     let res = result.split("&")
-    addable = '<tr>'
-        +`<td class="head_title">${res[0]}</td>`
-        +`<td class="head_title">${res[1]}</td>`
-        +`<td class="head_title">${res[2]}</td>`
-        +`<td class="head_title">${res[3]}</td>`
-        +`<td class="head_title">${res[4]}</td>`
-        +`<td class="head_title">${res[5]}</td>`
-        +'</tr>'
-    table.append(addable)
+    let addtable = '<tr class="deletebale">' + `<td class="head_title">${res[0]}</td>`
+    + `<td class="head_title">${res[1]}</td>` + `<td class="head_title">${res[2]}</td>`
+    + `<td class="head_title">${res[3]}</td>` + `<td class="head_title">${res[4]}</td>`
+    + `<td class="head_title">${res[5]}</td>`
+    + '</tr>'
+    table.append(addtable)
   }
 }
 
@@ -20,12 +21,8 @@ window.onload = function () {
   }
 }
 
-const MIN_Y = -5;
-const MAX_Y = 3;
-var rParametr = 0;
-
 function validateAndSend() {
-  var y = $('input[name="yCord"]').val();
+  let y = $('input[name="yCord"]').val();
   if((y > MIN_Y)&&(y < MAX_Y)&&(y !== "")){
     sendAndGetAjax()
   } else {
@@ -37,13 +34,6 @@ $('.send').click(function () {
   rParametr = this.value
   validateAndSend()
 })
-
-
-
-// function test(){
-//   let info = `xCord=${$('input[name="xCord"]:checked').val()}&yCord=${$('input[name="yCord"]').val()}&rParametr=${rParametr}`
-//   alert(info)
-// }
 
 function sendAndGetAjax() {
   $.ajax({
@@ -63,6 +53,7 @@ function sendAndGetAjax() {
 
 function clearTable() {
   $(".deletebale").remove()
+  localStorage.clear()
 }
 
 
